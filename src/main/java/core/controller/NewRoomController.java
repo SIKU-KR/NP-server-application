6ad9b431b49bb56de7ba58e90fee6801b9aa95be
mr.moeny <17997648qjc@gmail.com>
2024-11-01 11:56:52 +0800

    Update NewRoomController.java
    안녕하세요, 번역 완료되었습니다.

--- src/main/java/core/controller/NewRoomController.java
+++ src/main/java/core/controller/NewRoomController.java
@@ -5,11 +5,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- * ListRoomController manages TCP/IP connection for Clients try to get List of ChatRooms
- * 1. When Clients connect to socket, runs a thread to create new chatroom.
- * 2. Thread writes information from database using RoomModel instance
- * 3. Redirect to ChatController for client to enter new chat room
- * 4. Disconnects connection after finishing task
+ * 대화방 목록을 가져오려는 클라이언트를 위해 ListRoomController가 TCP/IP 연결을 관리합니다
+ *1. 클라이언트가 소켓에 연결하면 스레드를 실행하여 새 채팅방을 만듭니다.
+ *2. RoomModel 인스턴스를 사용하여 데이터베이스에서 스레드 쓰기 정보
+ *3. 클라이언트가 새 채팅방에 들어갈 수 있도록 채팅 컨트롤러로 리디렉션
+ *4. 작업 완료 후 연결 끊기
  */
 public class NewRoomController {
 
